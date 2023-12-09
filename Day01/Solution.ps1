85ef968541081241fa4b8a862adffecdac79fb7e
David Henry <ddude2410@live.com>
2023-12-08 20:14:24 -0500

    Solution 2 finished

--- Day01/Solution.ps1
+++ Day01/Solution.ps1
@@ -1,10 +1,48 @@
-$Data = Get-Content (Join-Path $PSScriptRoot -ChildPath "PuzzleInput.txt")
+$Data = Get-Content (Join-Path $PSScriptRoot -ChildPath "PuzzleData.txt")
 $Solution = 0
 
+$NumberLookup = @{
+    'one' = '1'
+    'two' = '2'
+    'three' = '3'
+    'four' = '4'
+    'five' = '5'
+    'six' = '6'
+    'seven' = '7'
+    'eight' = '8'
+    'nine' = '9'
+    "0" = '0'
+    "1" = '1'
+    "2" = '2'
+    "3" = '3'
+    "4" = '4'
+    "5" = '5'
+    "6" = '6'
+    "7" = '7'
+    "8" = '8'
+    "9" = '9'
+}
+
 foreach ($line in $Data) {
-    [string]$NumbersOnlyLine = $line -replace '[^\d]',''
-    [string]$OutputValue = $NumbersOnlyLine[0] + $NumbersOnlyLine[-1]
-    $Solution += $OutputValue
+    $Indexes = [System.collections.generic.list[PSCustomObject]]::new()
+
+    foreach ($key in $NumberLookup.Keys) {
+        $FirstIndex = $line.IndexOf($key)
+        $LastIndex = $line.LastIndexOf($key)
+        if ($FirstIndex -ne -1 -and $LastIndex -ne -1) {
+            $Object = [PSCustomObject]@{
+                KeyName = $key
+                FirstIndex = $FirstIndex
+                LastIndex = $LastIndex
+            }
+            $Indexes.Add($Object)
+        }
+    }
+    
+    [string]$FirstNumber = $NumberLookup[($Indexes | Sort-Object -Property FirstIndex)[0].KeyName]
+    [string]$LastNumber = $NumberLookup[($Indexes | Sort-Object -Descending -Property LastIndex)[0].KeyName]
+    $LineValue = ($FirstNumber + $LastNumber)
+    $Solution += $LineValue
 }
 
 return $Solution
